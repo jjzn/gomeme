06fac6795e67c531ae815ee7d7ff50708f17423b
Joshua Zien <50965831+jjzn@users.noreply.github.com>
2019-10-13 16:43:44 +0200

    implement meme downloader

--- gomeme.go
+++ gomeme.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+        "net/http"
+        "io/ioutil"
 )
 
 func main() {
@@ -35,6 +37,24 @@ func printHelp() {
 }
 
 func getMeme() {
+        if len(os.Args) < 3 {
+                fmt.Println("Error: no URL argument passed")
+                os.Exit(1)
+        }
+        res, err := http.Get(os.Args[2])
+        if err != nil {
+                defer fmt.Println("Common error causes include missing internet connection, server errrors or wrong URLs.")
+                fmt.Println("Error: network error (cannot get resource).")
+                os.Exit(1)
+        }
+        defer res.Body.Close()
+
+        data, err := ioutil.ReadAll(res.Body)
+        if err != nil {
+                fmt.Println("Error: read error (cannot read HTTP response)")
+                os.Exit(1)
+        }
+        fmt.Println(len(data))
 }
 
 func printError() {
