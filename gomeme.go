9ef9d4a9567dedaf2ce3bf6feadeb79b3ebc3e50
Joshua Zien <50965831+jjzn@users.noreply.github.com>
2019-10-13 13:15:00 +0200

    add help text

--- gomeme.go
+++ gomeme.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-        fmt.Println("Welcome to goMeme")
+	if len(os.Args) < 2 {
+		printError()
+		os.Exit(0)
+	}
+
+	switch os.Args[1] {
+	case "help":
+		printHelp()
+	case "get":
+		getMeme()
+	default:
+		printError()
+	}
+}
+
+const helpText = `
+Usage: gomeme command [arguments]
+
+Commands:
+    help:
+        Show this help text
+    get url:
+        Download the meme at url and save it to $HOME/memes
+`
+func printHelp() {
+	fmt.Println(helpText)
+}
+
+func getMeme() {
+}
+
+func printError() {
+	fmt.Println("Unknown command. Try gomeme help.")
 }
